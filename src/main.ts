23247ce2691f16bb4786f450d75f42150d653149
janvrska <1644599+janvrska@users.noreply.github.com>
2024-10-04 16:17:24 +0200

    Update to work with node20

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 async function run() {
   try {
-    const token = core.getInput('repo-token', {required: true});
-    const tag = core.getInput('tag', {required: true});
-    const sha = core.getInput('commit-sha', {required: false}) || github.context.sha;
+    const token = core.getInput("repo-token", { required: true });
+    const tag = core.getInput("tag", { required: true });
+    const sha =
+      core.getInput("commit-sha", { required: false }) || github.context.sha;
 
-    const client = new github.GitHub(token);
+    const client = github.getOctokit(token);
 
     core.debug(`tagging #${sha} with tag ${tag}`);
-    await client.git.createRef({
+    await client.rest.git.createRef({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       ref: `refs/tags/${tag}`,
       sha: sha
     });
-
   } catch (error) {
-    core.error(error);
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.error(error);
+      core.setFailed(error.message);
+    } else {
+      core.error(String(error));
+      core.setFailed("An unknown error occurred");
+    }
   }
 }
 
